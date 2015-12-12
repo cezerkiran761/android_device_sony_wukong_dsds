ce0088d93511a8ac221ec32a5906cd1301fdf99a
cezerkiran761 <cezerkiran761@gmail.com>
2015-12-12 11:33:26 +0530

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -12,38 +12,47 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Resolution
-TARGET_SCREEN_HEIGHT := 1280
-TARGET_SCREEN_WIDTH := 720
-
-# Kernel properties
+# Kernel
 TARGET_KERNEL_SOURCE := kernel/sony/wukong
-TARGET_KERNEL_CONFIG := yukon_wukong_defconfig
 
 # Assert
-TARGET_OTA_ASSERT_DEVICE := D2533,wukong
+TARGET_OTA_ASSERT_DEVICE := D2502,wukong,wukong_dsds
+
+# DSDS specific properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.radio.multisim.config=dsds \
+    persist.multisim.config=dsds \
+    telephony.lteOnCdmaDevice=0 \
+    ro.telephony.default_network=0,1
+
+# DSDS second ril start script
+PRODUCT_COPY_FILES += \
+    device/sony/wukong_dsds/rootdir/init.class_main.sh:root/init.class_main.sh
+
+# TWRP Recovery
+DEVICE_RESOLUTION := 720x1280
+BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_10x18.h\"
+
+# Inherit AOSP Yukon common device parts
+$(call inherit-product, device/sony/wukong_dsds/aosp_d2502.mk)
+
+# Inherit Omni Yukon common device parts
+$(call inherit-product, device/sony/yukon-common/device_omni.mk)
+
+# Inherit Omni GSM telephony parts
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Inherit Omni product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit gapps
+$(call inherit-product, vendor/apps/vendor-google.mk)
 
 # Recovery
-TARGET_RECOVERY_FSTAB := device/sony/wukong/rootdir/fstab.yukon
+PRODUCT_COPY_FILES += \
+    device/sony/wukong_dsds/recovery/root/etc/bootrec-device:recovery/root/etc/bootrec-device \
+    device/sony/wukong_dsds/rootdir/twrp.fstab:recovery/root/etc/twrp.fstab
 
-# Inherit from wukong device
-$(call inherit-product, device/sony/wukong/aosp_d2533.mk)
-
-# Inherit from common resources
-$(call inherit-product, device/sony/common/resources.mk)
-
-# Inherit CM common stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-
-SOMC_CFG_SENSORS_ACCEL_BMA250 := yes
-SOMC_CFG_SENSORS_COMPASS_AK8963 := yes
-SOMC_CFG_SENSORS_LIGHT_LIBALS := yes
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=d2533 \
-    BUILD_FINGERPRINT=Sony/D2533/D2533:5.1.1/19.4.A.0.182/26220503:user/release-keys \
-    PRIVATE_BUILD_DESC="D2533-user 5.1.1 19.4.A.0.182 26220503 release-keys"
-
-
-PRODUCT_NAME := cm_wukong
-PRODUCT_DEVICE := wukong
+# Override Product Name for OmniROM
+PRODUCT_NAME := omni_wukong_dsds
+PRODUCT_MODEL := Xperia c3
